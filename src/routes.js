a8f82dca32d36a470dd9b94bc6d4a31bf005d36e
Evandro Ishikawa <evandroishikawa@usp.br>
2020-01-15 01:11:20 -0300

    add src folder and routes

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+
+const routes = Router();
+
+routes.get('/', (request, response) => {
+  return response.json(
+    {
+      message: 'Hello OmniStack!',
+    }
+  );
+});
+
+module.exports = routes;
