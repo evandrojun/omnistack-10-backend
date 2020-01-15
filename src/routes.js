68a98720c11a7532c75df4d57f3b37319e31842f
Evandro Ishikawa <evandroishikawa@usp.br>
2020-01-15 09:47:58 -0300

    adjust post method for developers

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,27 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Developer = require('./models/developer');
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json(
-    {
-      message: 'Hello OmniStack!',
-    }
-  );
+routes.post('/devs', async (request, response) => {
+  const { github_username, techs } = request.body;
+
+  const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+  const { name = login, avatar_url, bio } = apiResponse.data;
+
+  const techsArray = techs.split(',').map(tech => tech.trim());
+
+  const newDeveloper = await Developer.create({
+    name,
+    github_username,
+    bio,
+    avatar_url,
+    techs: techsArray,
+  });
+
+  return response.json(newDeveloper);
 });
 
 module.exports = routes;
