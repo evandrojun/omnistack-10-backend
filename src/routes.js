3276def7edc15ff86d9b6a23fbb9f5765444a843
Evandro Ishikawa <evandroishikawa@usp.br>
2020-01-20 01:12:02 -0300

    refactor using controllers

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,9 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Developer = require('./models/developer');
+
+const DeveloperController = require('./controllers/developers_controller')
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-  const { github_username, techs } = request.body;
-
-  const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-
-  const { name = login, avatar_url, bio } = apiResponse.data;
-
-  const techsArray = techs.split(',').map(tech => tech.trim());
-
-  const newDeveloper = await Developer.create({
-    name,
-    github_username,
-    bio,
-    avatar_url,
-    techs: techsArray,
-  });
-
-  return response.json(newDeveloper);
-});
+routes.post('/devs', DeveloperController.store);
 
 module.exports = routes;
