00aa2f3af37d41247cb9e68d4f6f4e12431adfab
Evandro Ishikawa <evandroishikawa@usp.br>
2020-01-20 01:11:50 -0300

    add location to developer model

--- src/models/developer.js
+++ src/models/developer.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
+const PointSchema = require('./utils/point')
+
 const DeveloperSchema = new mongoose.Schema({
   name: String,
   github_username: String,
   bio: String,
   avatar_url: String,
   techs: [String],
+  location: {
+    type: PointSchema,
+    index: '2dsphere',
+  },
 });
 
 module.exports = mongoose.model('Developer', DeveloperSchema);
